e87a9ca44c920d817ff11dc6d1ec0cb838d76778
peacewrld <40036934+peacewrld@users.noreply.github.com>
2022-03-12 22:51:48 -0500

    Modified Ignore File

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using DatingApp.Data;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        // api/users/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
